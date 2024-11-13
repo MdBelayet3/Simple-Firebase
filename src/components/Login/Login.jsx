0bfa2ff8fa1035221efe74af4efa651d758fd0f6
Md belayet <mdbelayethossain016@gmail.com>
2024-11-13 15:25:21 +0600

    github provider add

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,15 +1,44 @@
-import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { getAuth, GithubAuthProvider, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
 import app from "../../firebase/firebase.init";
+import { useState } from "react";
 const Login = () => {
 
+    // auth and provider
     const auth = getAuth(app);
-    const provider = new GoogleAuthProvider();
+    const googleProvider = new GoogleAuthProvider();
+    const gitHubProvider = new GithubAuthProvider();
 
-    const handleGoogleSingIn = () =>{
-        signInWithPopup(auth,provider)
+    // hooks
+    const [user, setUser] = useState(null);
+
+    //event handler function
+    const handleGoogleSignIn = () => {
+        signInWithPopup(auth, googleProvider)
+            .then(result => {
+                const loggedInUser = result.user;
+                console.log(loggedInUser);
+                setUser(loggedInUser);
+            })
+            .catch(error => {
+                console.log('error', error.message);
+            })
+    }
+    const handleSignUpBtn = () => {
+        signOut(auth)
+            .then(result => {
+                console.log(result)
+                setUser(null)
+            })
+            .catch(error => {
+                console.log('error', error.message)
+            })
+    }
+    const handleGitHubSignIn = () =>{
+        signInWithPopup(auth,gitHubProvider)
         .then(result =>{
-            const user = result.user;
-            console.log(user);
+            const loggedUser = result.user;
+            console.log(loggedUser);
+            setUser(loggedUser);
         })
         .catch(error =>{
             console.log('error', error.message);
@@ -18,7 +47,19 @@ const Login = () => {
 
     return (
         <div>
-            <button onClick={handleGoogleSingIn}>Google SingIn</button>
+            { user ?
+            <button onClick={handleSignUpBtn}>Sign Out</button> :
+            <div>
+                <button onClick={handleGoogleSignIn}>Google SignIn</button>
+                <button onClick={handleGitHubSignIn}>GitHub SignIn</button>
+            </div>
+            }
+            {user &&
+                <div>
+                    <h3>User : {user.displayName}</h3>
+                    <p>Email : {user.email}</p>
+                    <img src={user.photoURL} alt="" />
+                </div>}
         </div>
     );
 };
